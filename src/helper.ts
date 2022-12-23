a37b2e0c154f290009239e333ee65d3608dc0e5f
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-12-23 18:00:57 +0530

    feat: TheaERC1155 subgraph mappings

---
+++ src/helper.ts
@@ -0,0 +1,65 @@
+import { address, integer } from '@protofire/subgraph-toolkit';
+import { Address, BigInt, Bytes } from '@graphprotocol/graph-ts';
+import { TheaERC1155, TheaERC1155Balance, Token, User } from '../generated/schema';
+
+const THEA_ERC1155 = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9';
+
+export function createUser(address: Address): User {
+  let user = User.load(address.toHex());
+  if (user === null) {
+    user = new User(address.toHex());
+    user.save();
+  }
+  return user;
+}
+
+export function fetchTheaErc1155(): TheaERC1155 {
+  let theaErc1155 = TheaERC1155.load(THEA_ERC1155);
+  if (theaErc1155 === null) {
+    theaErc1155 = new TheaERC1155(THEA_ERC1155);
+    theaErc1155.baseURI = '';
+  }
+  return theaErc1155;
+}
+
+export function createToken(tokenId: BigInt): Token {
+  const theaErc1155 = fetchTheaErc1155();
+  let token = Token.load(tokenId.toString());
+  if (token === null) {
+    token = new Token(tokenId.toString());
+    token.tokenURI = `${theaErc1155.baseURI}${tokenId.toString()}.json`;
+    token.totalSupply = integer.ZERO;
+  }
+  return token;
+}
+
+export function createTheaErc1155Balance(user: string, tokenId: string): TheaERC1155Balance {
+  let theaErc1155Balance = TheaERC1155Balance.load(user.concat('-').concat(tokenId));
+  if (theaErc1155Balance === null) {
+    theaErc1155Balance = new TheaERC1155Balance(user.concat('-').concat(tokenId));
+    theaErc1155Balance.token = tokenId;
+    theaErc1155Balance.owner = user;
+    theaErc1155Balance.amount = integer.ZERO;
+  }
+  return theaErc1155Balance;
+}
+
+export function updateBalanceOnTransfer(from: Address, to: Address, tokenId: string, amount: BigInt): void {
+  if (!address.isZeroAddress(from)) {
+    const fromUser = createUser(from);
+    const fromBalance = createTheaErc1155Balance(fromUser.id, tokenId);
+    fromBalance.amount = fromBalance.amount.minus(amount);
+    fromBalance.save();
+  }
+
+  if (!address.isZeroAddress(to)) {
+    const toUser = createUser(to);
+    const toBalance = createTheaErc1155Balance(toUser.id, tokenId);
+    toBalance.amount = toBalance.amount.plus(amount);
+    toBalance.save();
+  }
+}
+
+export function hexToBI(hexString: string): BigInt {
+  return BigInt.fromUnsignedBytes(changetype<Bytes>(Bytes.fromHexString(hexString).reverse()));
+}
---
+++ src/mappings.ts
@@ -0,0 +1,101 @@
+import { TransferSingle, TransferBatch, URI } from './../generated/TheaERC1155/TheaERC1155';
+import { Converted, Recovered } from './../generated/BaseTokenManager/BaseTokenManager';
+import {
+  Retired as RetireEvent,
+  Converted as ConvertEvent,
+  Recovered as RecoverEvent,
+  Unwraped,
+} from './../generated/schema';
+import { Tokenized, Retired, UpdatedUnwrapRequest } from './../generated/Registry/Registry';
+import { createToken, createUser, fetchTheaErc1155, hexToBI, updateBalanceOnTransfer } from './helper';
+
+export function handleTokenized(event: Tokenized): void {
+  const theaErc1155 = fetchTheaErc1155();
+  theaErc1155.save();
+  createUser(event.params.to);
+
+  const token = createToken(event.params.tokenId);
+  token.contract = theaErc1155.id;
+  token.projectId = hexToBI(event.params.projectID.toHexString());
+  token.vintage = hexToBI(event.params.vintage.toHexString());
+  token.totalSupply = token.totalSupply.plus(event.params.amount);
+  token.save();
+}
+
+export function handleRetired(event: Retired): void {
+  const user = createUser(event.params.from);
+
+  const token = createToken(event.params.id);
+  token.totalSupply = token.totalSupply.minus(event.params.amount);
+  token.save();
+
+  const retireEvents = token.retireEvents;
+  const retireEvent = new RetireEvent(token.id.concat('-').concat(retireEvents.length.toString()));
+  retireEvent.by = user.id;
+  retireEvent.token = token.id;
+  retireEvent.amount = event.params.amount;
+  retireEvent.timestamp = event.block.timestamp;
+  retireEvent.save();
+}
+
+export function handleConverted(event: Converted): void {
+  const user = createUser(event.transaction.from);
+  const token = createToken(event.params.tokenId);
+
+  const convertEvents = token.convertEvents;
+  const convertEvent = new ConvertEvent(token.id.concat('-').concat(convertEvents.length.toString()));
+  convertEvent.by = user.id;
+  convertEvent.token = token.id;
+  convertEvent.amount = event.params.amount;
+  convertEvent.timestamp = event.block.timestamp;
+  convertEvent.save();
+}
+
+export function handleRecovered(event: Recovered): void {
+  const user = createUser(event.transaction.from);
+  const token = createToken(event.params.tokenId);
+
+  const recoverEvents = token.recoverEvents;
+  const recoverEvent = new RecoverEvent(token.id.concat('-').concat(recoverEvents.length.toString()));
+  recoverEvent.by = user.id;
+  recoverEvent.token = token.id;
+  recoverEvent.amount = event.params.amount;
+  recoverEvent.timestamp = event.block.timestamp;
+  recoverEvent.save();
+}
+
+export function handleTransferSingle(event: TransferSingle): void {
+  const token = createToken(event.params.id);
+  updateBalanceOnTransfer(event.params.from, event.params.to, token.id, event.params.value);
+}
+
+export function handleTransferBatch(event: TransferBatch): void {
+  const tokenIds = event.params.ids;
+  const amounts = event.params.values;
+  for (let i = 0; i < tokenIds.length; i++) {
+    const token = tokenIds[i];
+    const amount = amounts[i];
+    updateBalanceOnTransfer(event.params.from, event.params.to, token.toString(), amount);
+  }
+}
+
+export function handleURI(event: URI): void {
+  const theaErc1155 = fetchTheaErc1155();
+  theaErc1155.baseURI = event.params.value;
+  theaErc1155.save();
+}
+
+export function handleUnwrap(event: UpdatedUnwrapRequest): void {
+  const user = createUser(event.params.owner);
+  const token = createToken(event.params.id);
+  token.totalSupply = token.totalSupply.minus(event.params.amount);
+  token.save();
+
+  const unwrapEvents = token.unwrapEvents;
+  const unwrapEvent = new Unwraped(token.id.concat('-').concat(unwrapEvents.length.toString()));
+  unwrapEvent.by = user.id;
+  unwrapEvent.token = token.id;
+  unwrapEvent.amount = event.params.amount;
+  unwrapEvent.timestamp = event.block.timestamp;
+  unwrapEvent.save();
+}
